3712e18ecb28e78ba2ff85557282f1d373998b3a
da3mon <da3mon>
2020-10-12 23:02:02 +0530

    added dependency installation script and color formatting to enum.sh

--- enum.sh
+++ enum.sh
@@ -4,30 +4,37 @@
 [[ -d "${HOME}/Recon_data" ]] || mkdir "${HOME}/Recon_data"
 
 ##Prompt for overwriting existing domain directory.
-[[ -d "${HOME}/Recon_data/${1}" ]] && printf "Directory ${HOME}/Recon_data/${1} already exists.Do you want to overwrite[y/N]?" && read ans && [[ "$ans" != "y" ]] && exit || rm -r "${HOME}/Recon_data/${1}"
+ans="N"
+[[ -d "${HOME}/Recon_data/${1}" ]] && printf "Directory ${HOME}/Recon_data/${1} already exists.Do you want to overwrite[y/N]?" && read ans && [[ "$ans" != "y" ]] && exit
+[[ $ans == "y" ]] && rm -r "${HOME}/Recon_data/${1}"
 mkdir "${HOME}/Recon_data/${1}"
 
+#Color formating vars
+RED='\033[0;31m'
+NC='\033[0m'
+
 ##Passive Subdomain enumeration.
-echo "[+] Started passive Enumeration using findomain"
+echo -e "${RED}[+] Started passive Enumeration using findomain${NC}"
 findomain -c fd_config.ini -q -t "${1}"  -u "${HOME}/Recon_data/${1}/${1}.subs"
-echo "[+] Started passive Enumeration using assetfinder"
+echo -e "${RED}[+] Started passive Enumeration using assetfinder${NC}"
 assetfinder -subs-only "${1}" | anew "${HOME}/Recon_data/${1}/${1}.subs" 
-echo "[+] Started passive Enumeration using subfinder"
+echo -e "${RED}[+] Started passive Enumeration using subfinder${NC}"
 subfinder -d "${1}" -silent | anew "${HOME}/Recon_data/${1}/${1}.subs" 
 
 ##Getting subdomains from Rapid7 FDNS Dataset.
-echo "[+] Getting Subdomains from FDNS Dataset"
+echo -e "${RED}[+] Getting Subdomains from FDNS Dataset${NC}"
 crobat -s "${1}" | grep "${1}" |  anew "${HOME}/Recon_data/${1}/${1}.subs" 
 
 ##Updating CommonSpeak2 Wordlist.
 [[ -f etag.txt ]] || touch etag.txt
-echo "[+] Checking for Commonspeak2 Wordlist Updates"
+echo -e "${RED}[+] Checking for Commonspeak2 Wordlist Updates${NC}"
 etag=$( curl -I -s https://raw.githubusercontent.com/assetnote/commonspeak2-wordlists/master/subdomains/subdomains.txt | grep -i "Etag" | cut -f 2 -d " " | sed "s/\"//g" )
-[[ $etag != $( cat etag.txt ) ]] && echo "[+] Updating Wordlist" && echo $etag > etag.txt && wget -q  https://raw.githubusercontent.com/assetnote/commonspeak2-wordlists/master/subdomains/subdomains.txt && echo "[+] Update Completed"
+[[ $etag != $( cat etag.txt ) ]] && echo -e "${RED}[+] Updating Wordlist${NC}" && echo $etag > etag.txt && wget -q  https://raw.githubusercontent.com/assetnote/commonspeak2-wordlists/master/subdomains/subdomains.txt && echo -e "${RED}[+] Update Completed${NC}"
 
 ##Refreshing Resolvers list.
+[[ -f "resolvers.txt" ]] || dnsvalidator -tL https://public-dns.info/nameservers.txt -threads 100 -o resolvers.txt
 [[ "${2}" == "refresh" ]] && dnsvalidator -tL https://public-dns.info/nameservers.txt -threads 100 -o resolvers.txt
 
 ##Bruteforcing Subdomains using shuffledns.
-echo "[+] Started subdomain bruteforcing"
+echo -e "${RED}[+] Started subdomain bruteforcing${NC}"
 shuffledns -silent -d "${1}" -w subdomains.txt  -r resolvers.txt -o "${HOME}/Recon_data/${1}/final_subs.txt"
---
+++ install.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+##Install dependencies
+sudo apt update -y && sudo apt install -y build-essential make rustc perl git software-properties-common
+
+##Install python
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update -y && sudo apt install python3.8
+
+##Install findomain
+git clone https://github.com/Edu4rdSHL/findomain.git
+cd findomain
+cargo build --release
+sudo mv target/release/findomain /usr/bin/
+cd $HOME
+
+##Install Golang
+wget https://dl.google.com/go/go1.15.2.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.15.2.linux-amd64.tar.gz
+echo "export PATH=\"${PATH}:/usr/local/go/bin\"" >> $HOME/.profile
+echo "export GOPATH=\"${PATH}:${HOME}/go/bin\"" >> $HOME/.profile
+source $HOME/.profile
+
+##Install assetfinder
+go get -u github.com/tomnomnom/assetfinder
+
+##Install anew
+go get -u github.com/tomnomnom/anew
+
+##Install subfinder
+GO111MODULE=on go get -u -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+
+##Install crobat
+go get -u github.com/cgboal/sonarsearch/crobat
+
+##Install dnsvalidator
+https://github.com/vortexau/dnsvalidator.git
+cd dnsvalidator/
+python3 setup.py install
+cd $HOME
+
+##Install massdns
+https://github.com/blechschmidt/massdns.git
+cd massdns/ && make && sudo mv bin/massdns /usr/bin/
+cd $HOME
+
+##Install shuffledns
+GO111MODULE=on go get -u -v github.com/projectdiscovery/shuffledns/cmd/shuffledns
